492b0fd2f0ca6ee14f3c4e2e8db12240689d8910
jvaldezm1 <jvaldezm1@miumg.edu.gt>
2023-06-17 07:42:00 -0600

    creacion de documento Mostrardatos y se crea un interface de botones

---
+++ JavaApplication14/src/javaapplication14/MostrarDatos.java
@@ -0,0 +1,15 @@
+
+package javaapplication14;
+
+public class MostrarDatos {
+
+   
+    public static void main(String[] args) {
+       new NewJFrame().setVisible(true);//commit 1 
+       
+       
+       
+       
+    }
+    
+}
